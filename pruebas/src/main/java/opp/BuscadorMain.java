d8b947aea9a454194343f3c2d1694f862593e9d9
Your Name <sergio_793@hotmail.com>
2021-11-26 13:45:42 -0500

    Clase 6 -- POO y Funciones

---
+++ pruebas/src/main/java/opp/BuscadorMain.java
@@ -0,0 +1,15 @@
+package opp;
+
+public class BuscadorMain {
+	public static void main(String[] args) {
+		//www.mercadolibre.com.co
+		Buscador buscador = new Buscador();
+		
+		String claveDeBusqueda = "iron man";
+		buscador.definirClaveBusqueda(claveDeBusqueda);
+
+		buscador.buscar();
+		
+		buscador.mostrarResultados();
+	}
+}
