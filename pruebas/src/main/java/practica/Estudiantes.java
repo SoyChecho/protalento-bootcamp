9d4b625038aa96afa346ba3291993546db00e48d
Your Name <sergio_793@hotmail.com>
2021-11-29 10:57:17 -0500

    Estudiantes con Error en Array

--- pruebas/src/main/java/practica/Estudiantes.java
+++ pruebas/src/main/java/practica/Estudiantes.java
@@ -1,10 +1,33 @@
 package practica;
 
+import java.util.Scanner;
+
 public class Estudiantes {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String estudiante;
+		 Scanner leer = new Scanner(System.in);
+		    int numEstudiantes;
+		    String[] estudiantes;
+		    //double suma=0, promedio, max, min;
+		    //int numAprobados=0, numSuspendidos=0;
+		    //---
+		    // Array Estudiantes
+		    System.out.print("Numero de estudiantes: ");
+		    numEstudiantes = leer.nextInt();
+		    estudiantes = new String[numEstudiantes];
+
+		    System.out.println("Ingrese un total de " + numEstudiantes + " estudiantes con nombre y apellido seguido de la tecla enter: ");
+		    for(int i=0; i<estudiantes.length; i++){
+		        System.out.print("Estudiante "+ (i+1) + ": ");
+		        
+		        estudiantes[i] = leer.nextLine();
+		    }
+		    
+		    System.out.println("Los estudiantes a evaluar: ");
+		    for (String estudiante : estudiantes) {
+		        System.out.print(estudiante + "   ");
+		    }
+		    leer.close();
 	}
 
 }
