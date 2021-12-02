5fc0ab0daf5ff3fa58338543fe757470b32217d2
Your Name <sergio_793@hotmail.com>
2021-12-02 07:14:23 -0500

    Clase 7: ----- App Web

--- pruebas/src/main/java/opp/Buscador.java
+++ pruebas/src/main/java/opp/Buscador.java
@@ -1,33 +1,69 @@
 package opp;
 
 public class Buscador {
-	String claveBusqueda;
-	Articulo[] resultados;
-	
-	Buscador() {
+	//atributos
+		//modificadores
+		private String claveBusqueda;
+		private Articulo[] resultados;
 		
-	}
-	
-	void buscar() {
-		System.out.println("buscando..." + this.claveBusqueda);
+		private Integer cantidadResultados;
 		
-		// como mi buscador necesita usar un objeto de la clase DB
-		// debe instanciar un objeto de dicha clase
-		DB db = new DB();
+		//public Integer unAtributo; //NO SE HACE!!!
 		
-		this.resultados = db.consultar(claveBusqueda);
-	}
-	
-	void definirClaveBusqueda(String clave) {
-		claveBusqueda = clave;
-	}
-	
-	void mostrarResultados() {
-		if(this.resultados.length > 0) {
-			for(Articulo aux : this.resultados ) {
-				aux.detalleArticulo();
+		//CONSTRUCTOR
+		public Buscador() {
+			System.out.println("Creando buscador...");
+		}
+		
+		public void buscar() {
+			System.out.println("buscando..." + this.claveBusqueda);
+			
+			// como mi buscador necesita usar un objeto de la clase DB
+			// debe instanciar un objeto de dicha clase
+			DB db = new DB();
+			
+			this.resultados = db.consultar(claveBusqueda);
+
+			//invocamos a un metodo interno (privado) pasando el tamaño del vector como parametro
+			this.setCantidadResultados(this.resultados.length);
+		}
+		
+		private void setCantidadResultados(int cantidad) {
+			if(cantidad < 0 ) {
+				this.cantidadResultados = 0;
+			}else {
+				this.cantidadResultados = cantidad;
 			}
-		}else {
-			System.out.println("No hay resultados para " + this.claveBusqueda);
-		}		}
+		}
+		
+		public Integer getCantidadResultados() {
+			if(this.cantidadResultados == null) {
+				this.setCantidadResultados(0);
+			}
+			return this.cantidadResultados;
+		}
+		
+		//alt+shit+s
+		
+		public void mostrarResultados() {
+			if(this.resultados.length > 0) {
+				for(Articulo aux : this.resultados ) {
+					aux.detalleArticulo();
+				}
+			}else {
+				System.out.println("No hay resultados para " + this.claveBusqueda);
+			}
+		}
+
+		public String getClaveBusqueda() {
+			return claveBusqueda;
+		}
+
+		public void setClaveBusqueda(String claveBusqueda) {
+			this.claveBusqueda = claveBusqueda;
+		}
+
+		public Articulo[] getResultados() {
+			return resultados;
+		}
 }
