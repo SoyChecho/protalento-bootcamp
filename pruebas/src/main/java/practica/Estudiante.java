21815b0a15f09bb1af8d15d6f8501a62f915f857
Your Name <sergio_793@hotmail.com>
2021-12-03 08:54:17 -0500

    trtando de hacerlo en clases

--- pruebas/src/main/java/practica/Estudiante.java
+++ pruebas/src/main/java/practica/Estudiante.java
@@ -6,12 +6,30 @@ public class Estudiante {
 	private String apellido;
 	private int nota;
 	
-	public Estudiante() {
-		nombre = "";
-		apellido = "";
-		nota = 0;
+	public Estudiante(String nombre,String apellido,int nota) {
+		
 	}
+	
+	public void xEstudiante() {
+		System.out.print("Numero de estudiantes: ");
+	    numEstudiantes = leer.nextInt();
+	    estudiantes = new String[numEstudiantes];
+	    
+	    leer.nextLine();
+	    
+	    System.out.println("Ingrese un total de " + numEstudiantes + " estudiantes con nombre y apellido seguido de la tecla enter: ");
+	    for(int i=0; i<estudiantes.length; i++){
+	        System.out.print("Estudiante "+ (i+1) + ": ");
+	        
+	        estudiantes[i] = leer.nextLine();
+	    }
 
+
+	    }
+
+
+	}
+		
 	public String getNombre() {
 		return nombre;
 	}
